2cf14f42eeb2acb7d678cae2ba4d0e7aa20d8e4b
Johnil Quezada <johnilquezada@gmail.com>
2024-11-13 23:03:12 +0000

    cleanup generated file, add repo info

--- tools/nx-build/src/executors/bundle/executor.d.ts
+++
@@ -1,8 +0,0 @@
-import type { ExecutorContext } from '@nx/devkit';
-export interface BundleExecutorOptions {
-    package: string;
-    entryPointModule: string;
-}
-export default function bundleExecutor(options: BundleExecutorOptions, context: ExecutorContext): Promise<{
-    success: boolean;
-}>;
